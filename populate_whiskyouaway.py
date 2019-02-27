42a9f0a220d22f31429c445c42e77bdd2f3ce021
Nicki Buckley <0602452b@student.gla.ac.uk>
2019-02-27 12:54:28 +0000

    More populate changes

--- populate_whiskyouaway.py
+++ populate_whiskyouaway.py
@@ -12,7 +12,33 @@ def populate():
 	"url": "https://trnsmtfest.com/"},
 	{"title": "King Tut's Wah Wah Hut",
 	"url": "https://www.kingtuts.co.uk/"},
-	{"title": "https://tartanheartfestival.co.uk/"} ]
+	{"title": "Tartan Heart Festival",
+	"url": "https://tartanheartfestival.co.uk/"} ]
+
+	cats = {"Music": {"events": music_pages}, } 
+
+	for cat, cat_data in cats.items():
+		c = add_cat(cat)
+		for e in cat_data["events"]:
+			add_event(c, e["title"], e["url"])
+
+	# print the added categories
+
+	for c in Category.objects.all():
+		for e in Event.objects.filter(category=c):
+			print("- {0} - {1}".format(str(c), str(e)))
+
+def add_event(cat, title, url, views=0):
+	e = Event.objects.get_or_create(category=cat, title=title)[0]
+	e.url=url
+	e.views=views
+	e.save()
+	return e
+
+def add_cat(name):
+	c = Category.objects.get_or_create(name=name)[0]
+	c.save()
+	return c
 
 
 if __name__ == '__main__':
