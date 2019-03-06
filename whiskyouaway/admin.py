ad148a97b82e90b0270544ba3231bbcd873de859
Jessica Fong <2138525F@student.gla.ac.uk>
2019-03-06 10:24:06 +0000

    added user stuff into admin.py

--- whiskyouaway/admin.py
+++ whiskyouaway/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from whiskyouaway.models import Category, Event, CategoryContainer
+from whiskyouaway.models import Category, Event, CategoryContainer,UserProfile, Review
 
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Event)
 admin.site.register(CategoryContainer)
-# admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(Review)
