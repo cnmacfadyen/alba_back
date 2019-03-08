44ac93afec19faa9007441181f2378706ec9e357
Jessica Fong <2138525F@student.gla.ac.uk>
2019-03-08 11:40:47 +0000

    registration, login, logout, change password sorted

--- whiskyouaway/views.py
+++ whiskyouaway/views.py
@@ -64,53 +64,53 @@ def show_category(request, category_name_slug):
 		context_dict['events'] = None
 	return render(request, 'whiskyouaway/categories.html', context_dict)
 
-def register(request):
-	registered = False
-	if request.method == 'POST':
-		user_form = UserForm(data=request.POST)
-		profile_form = UserProfileForm(data=request.POST)
+#def register(request):
+#	registered = False
+#	if request.method == 'POST':
+#		user_form = UserForm(data=request.POST)
+#		profile_form = UserProfileForm(data=request.POST)
+#
+#		if user_form.is_valid() and profile_form.is_valid():
+#			user = user_form.save()
+#			user.set_password(user.password)
+#			user.save()
+#			profile = profile_form.save(commit=False)
+#			profile.user = user
+#
+#			if 'picture' in request.FILES:
+#				profile.picture = request.FILES['picture']
+#			profile.save()
+#			registered = True
+#		else:
+#			print(user_form.errors, profile_form.errors)
+#	else:
+#		user_form = UserForm()
+#		profile_form = UserProfileForm()
+#
+#	return render(request, 'whiskyouaway/register.html', {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
-		if user_form.is_valid() and profile_form.is_valid():
-			user = user_form.save()
-			user.set_password(user.password)
-			user.save()
-			profile = profile_form.save(commit=False)
-			profile.user = user
+#def user_login(request):
+#	if request.method == 'POST':
+#		username = request.POST.get('username')
+#		password = request.POST.get('password')
+#		user = authenticate(username=username, password=password)
+#		if user:
+#			if user.is_active:
+#				login(request, user)
+#				return HttpResponseRedirect(reverse('index'))
+#			else:
+#				return HttpResponse("Your account is disabled.")
+#		else:
+#			print("Invalid login details: {0}, {1}".format(username, password))
+#			return HttpResponse("Invalid login details supplied.")
 
-			if 'picture' in request.FILES:
-				profile.picture = request.FILES['picture']
-			profile.save()
-			registered = True
-		else:
-			print(user_form.errors, profile_form.errors)
-	else:
-		user_form = UserForm()
-		profile_form = UserProfileForm()
+#	else:
+#		return render(request, 'whiskyouaway/login.html', {})
 
-	return render(request, 'whiskyouaway/register.html', {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
-
-def user_login(request):
-	if request.method == 'POST':
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		user = authenticate(username=username, password=password)
-		if user:
-			if user.is_active:
-				login(request, user)
-				return HttpResponseRedirect(reverse('index'))
-			else:
-				return HttpResponse("Your account is disabled.")
-		else:
-			print("Invalid login details: {0}, {1}".format(username, password))
-			return HttpResponse("Invalid login details supplied.")
-
-	else:
-		return render(request, 'whiskyouaway/login.html', {})
-
-@login_required
-def user_logout(request):
-	logout(request)
-	return HttpResponseRedirect(reverse('index'))
+#@login_required
+#def user_logout(request):
+#	logout(request)
+#	return HttpResponseRedirect(reverse('index'))
 
 # animal pages
 
